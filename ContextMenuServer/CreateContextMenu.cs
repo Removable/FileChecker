096bd021a87a8272088647bc1aaa56ef6e604d17
Removable <me@imguan.com>
2018-10-29 23:34:52 +0800

    分离项目内容

--- ContextMenuServer/CreateContextMenu.cs
+++ ContextMenuServer/CreateContextMenu.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using SharpShell.Attributes;
 using SharpShell.SharpContextMenu;
 
-namespace FileChecker.Util
+namespace ContextMenuServer
 {
     [ComVisible(true)]
     [COMServerAssociation(AssociationType.AllFiles)]
